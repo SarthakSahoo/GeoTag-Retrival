576f6ea27bcf6daabdf42a84938185ebdd0e09b5
Sarthak Kumar Sahoo <31481715+SarthakSahoo@users.noreply.github.com>
2021-04-17 13:14:29 +0530

    Update geoloc.py

--- geoloc.py
+++ geoloc.py
@@ -8,14 +8,17 @@ from collections import namedtuple
 
 # Function to convert GPS coordinated into degree format
 def _convert_to_degress(value):
+    # Degree
     d0 = value[0][0]
     d1 = value[0][1]
     d = float(d0) / float(d1)
     
+    # Minute
     m0 = value[1][0]
     m1 = value[1][1]
     m = float(m0) / float(m1)
     
+    # Second
     s0 = value[2][0]
     s1 = value[2][1]
     s = float(s0) / float(s1)
@@ -31,36 +34,53 @@ def _get_if_exist(data,key):
 
 # Function to return the longitude and latitude
 def get_lat_lon(exif_data):
+    # Defining initial value of latitude and longitude data as None
     lat = None
     lon = None
     
+    # If GPSInfo is present in our retrieved exif data then retrieve all other attribute of exif data
     if "GPSInfo" in exif_data:
+        # Retrieving through GPSInfo key in exif data
         gps_info = exif_data["GPSInfo"]
         
+        # Retrieving the Latitude, Longitude, LatitudeRef and LongitudeRef from our data if it exists
+        # If you want to know what all these data are please check the link below:
+        # Reference: https://exiftool.org/TagNames/GPS.html
         gps_latitude = _get_if_exist(gps_info, "GPSLatitude")
         gps_latitude_ref = _get_if_exist(gps_info, 'GPSLatitudeRef')
         gps_longitude = _get_if_exist(gps_info, 'GPSLongitude')
         gps_longitude_ref = _get_if_exist(gps_info, 'GPSLongitudeRef')
         
+        # If we get all the coordinates correctly then converting these GPS data into degree format
         if gps_latitude and gps_latitude_ref and gps_longitude and gps_longitude_ref:
             lat = _convert_to_degress(gps_latitude)
+            # If Latitude ref is not North
             if gps_latitude_ref != "N":                     
                 lat = 0 - lat
-    
+            # If Longitude ref is not East
             lon = _convert_to_degress(gps_longitude)
             if gps_longitude_ref != "E":
                 lon = 0 - lon
-                
+    # Returning Our Final data        
     return lat, lon
 
 # Function to return the exif data in the form of a directory
 def get_exif_data(image):
+    '''
+    EXIF data is stored data in an image file that includes the date the photo was taken, resolution, shutter speed, focal length, and other camera settings. 
+    Getting the EXIF data for an image results in a dictionary indexed by EXIF numeric tags.
+    '''
     info = image._getexif()
+    # If there is no exif data then return None
     if(info is None):
         return info
     exif_data = {}
+    # For the different tags and value we got from our exif data 
+    # Let's get GPSInfo and other required value which we will use
     for tag, value in info.items():
+        # Getting the required data from the dictionary
         decoded = TAGS.get(tag, tag)
+        # Pulling out the GPS specific tags
         if(decoded == "GPSInfo"):
             gps_data = {}
             for t in value:
@@ -98,8 +118,12 @@ if __name__ == "__main__":
     # From image path list retriving the image and storing the GPS value in defined dictionary
     for i in range(0, len(image_path_list)):
         img = image_path_list[i]
+        # Opening the image
         image = Image.open(img)
+        # Retrieving the exif data from the image
         exif_data = get_exif_data(image)
+        # If exif data is there then retrieve the latitude and longitude data
+        # Process these data and store it in our requried dictionary
         if(exif_data == None):
             count+=1
             continue
